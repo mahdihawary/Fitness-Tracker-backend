dc8cef9e5ad5ba12e2090fd3e39782a2a9070d6b
mahdihawary <Mahdihawary@gmail.com>
2020-10-22 09:41:41 -0400

    latest stuff

--- app/controllers/api/v1/routine_exercises_controller.rb
+++ app/controllers/api/v1/routine_exercises_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::RoutineExercisesController < ApplicationController
+     def create
+        routine_exercise = Routine.create(routine_exercise_params)
+        render json: routine_exercise
+    end
+
+
+    private
+    def routine_exercise_params
+        params.require(:routine_exercise).permit(:routine_id, :exercise_id)
+    end
 end
