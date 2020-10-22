9398e760322ea993fcf10529b4670ed8bcbaacf6
jdhofmann <justindhofmann@gmail.com>
2020-10-22 10:35:05 -0400

    RoutineExercise create bug fix

--- app/controllers/api/v1/routine_exercises_controller.rb
+++ app/controllers/api/v1/routine_exercises_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::RoutineExercisesController < ApplicationController
-     def create
-        routine_exercise = Routine.create(routine_exercise_params)
+    def create
+        routine_exercise = RoutineExercise.create(routine_exercise_params)
         render json: routine_exercise
     end
 
-
     private
+
     def routine_exercise_params
-        params.require(:routine_exercise).permit(:routine_id, :exercise_id)
+        params.require(:routine_exercise).permit(:exercise_id, :routine_id)
     end
 end
